40270391c854ed22be52e9d46a41d7ec83755258
Scott Doxey <neogeek0101@gmail.com>
2020-02-24 22:33:05 -0500

    Version bump.

--- find-unity.rb
+++ find-unity.rb
@@ -1,8 +1,8 @@
 class FindUnity < Formula
   desc "Command line tool for locating the version of Unity that a project was built with"
   homepage "https://github.com/neogeek/find-unity"
-  url "https://github.com/neogeek/find-unity/archive/v1.0.0.tar.gz"
-  sha256 "9e0b4ae941cbfae818d40283dc11d3c1692b92acb339c3783f4e982319cb2245"
+  url "https://github.com/neogeek/find-unity/archive/v2.0.0.tar.gz"
+  sha256 "68231c4e333092e314e7614e15127886ecf7eff99eb2c2f479b038fe09fb291c"
 
   def install
     bin.install "bin/find-unity"
